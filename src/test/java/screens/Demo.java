625348540fe63926e44fdb5fb8b31efb81586f82
pavitra g k <pavithrak@geekyants.com>
2022-06-16 10:50:02 +0530

    Integrated allure reports

--- src/test/java/screens/Demo.java
+++ src/test/java/screens/Demo.java
@@ -1,19 +1,19 @@
-package screens;
-
-import org.openqa.selenium.support.PageFactory;
-
-import io.appium.java_client.AppiumDriver;
-
-import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import tests.BaseClass;
-
-public class Demo {
-	
-	public static AppiumDriver driver;
-
-	public Demo(AppiumDriver driver) {
-		this.driver = driver;
-		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
-	}
-
-}
+//package screens;
+//
+//import org.openqa.selenium.support.PageFactory;
+//
+//import io.appium.java_client.AppiumDriver;
+//
+//import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+//import tests.BaseClass;
+//
+//public class Demo {
+//	
+//	public static AppiumDriver driver;
+//
+//	public Demo(AppiumDriver driver) {
+//		this.driver = driver;
+//		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+//	}
+//
+//}
--- src/test/java/tests/BaseClass.java
+++ src/test/java/tests/BaseClass.java
@@ -68,6 +68,7 @@ public class BaseClass {
 	
 	
 	
+	@SuppressWarnings("deprecation")
 	@BeforeTest
 	public void setup()  {
 		
